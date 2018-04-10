2c2f7bcfb8d3e18987c43a6307f51d4defa783b8
Sakura <1003126911@qq.com>
2018-04-10 19:25:16 +0800

    update h3

---
+++ homework3/ISSActionCallback.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SSActionEventType:int {Started, Completed}
+
+public interface ISSActionCallback {
+	void SSActionEvent(SSAction source);
+
+}
