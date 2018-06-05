da0ff0ad29b702bb2fc16e0f4a34bdf355e52fb5
Sakura <1003126911@qq.com>
2018-06-05 21:02:45 +0800

    Update h8

---
+++ homework8/README.md
@@ -0,0 +1,9 @@
+# Quest Log 公告牌
+---
+>使用 Unity3D 完成公告牌设计
+
+* 参考博客：
+    * [Unity3d学习笔记（8）-- 可展开公告牌](https://blog.csdn.net/x2_yt/article/details/71330103)
+    * [Unity3D协程介绍 以及 使用](https://blog.csdn.net/huang9012/article/details/38492937)
+
+* 视频地址（GitHub仓库里面也有）
---
+++ homework8/ToggleText.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleText : MonoBehaviour
+{
+
+    private Button yourButton;
+    public Text text;
+    private int frame = 20;
+
+    // Use this for initialization  
+    void Start()
+    {
+        Button btn = this.gameObject.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+        if (text.gameObject.activeSelf)             // at the begining the text can be see, so first rotate it and size it
+        {
+            float rx = 0;
+            float xy = 65;
+            text.transform.rotation = Quaternion.Euler(rx, 0, 0);
+            text.rectTransform.sizeDelta = new Vector2(text.rectTransform.sizeDelta.x, xy);
+            text.gameObject.SetActive(false);
+        }
+    }
+
+    IEnumerator rotateIn()
+    {
+        float rx = 0;
+        float xy = 65;
+        for (int i = 0; i < frame; i++)
+        {
+            rx -= 90f / frame;
+            xy -= 65f / frame;
+            text.transform.rotation = Quaternion.Euler(rx, 0, 0);
+            text.rectTransform.sizeDelta = new Vector2(text.rectTransform.sizeDelta.x, xy);
+            if (i == frame - 1)
+            {
+                text.gameObject.SetActive(false);
+            }
+            yield return null;
+        }
+    }
+
+    IEnumerator rotateOut()
+    {
+        float rx = -90;
+        float xy = 0;
+        for (int i = 0; i < frame; i++)
+        {
+            rx += 90f / frame;
+            xy += 65f / frame;
+            text.transform.rotation = Quaternion.Euler(rx, 0, 0);
+            text.rectTransform.sizeDelta = new Vector2(text.rectTransform.sizeDelta.x, xy);
+            if (i == 0)
+            {
+                text.gameObject.SetActive(true);
+            }
+            yield return null;
+        }
+    }
+
+
+    void TaskOnClick()
+    {
+        if (text.gameObject.activeSelf)
+        {
+            StartCoroutine(rotateIn());
+        }
+        else
+        {
+            StartCoroutine(rotateOut());
+        }
+
+    }
+}
